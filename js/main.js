14c9a9b85c7fdc2250b69615549513a181b2e5d9
kate <k_czarnota@wp.pl>
2023-10-24 12:54:46 +0200

    zadanie 47947

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function sprawdzLiczbe(n) {
+    if (n >= 100 && n <= 200) {
+        console.log("Liczba znajduje się w przedziale");
+    } else {
+        console.log("Liczba nie znajduje się w przedziale");
+    }
+}
+
+sprawdzLiczbe(125);
+/* sprawdzLiczbe(700); */
